2f1e2a259bf2b6932eb6325b6656ffb15101a5a9
xin <1226060012@qq.com>
2017-04-19 12:10:42 +0800

    22

--- src/views/car/car.vue
+++ src/views/car/car.vue
@@ -5,101 +5,96 @@
 <Breadcrumb-item href="/User">直通车管理</Breadcrumb-item>
 <Breadcrumb-item>车辆管理</Breadcrumb-item>
 </Breadcrumb>
-        <Form :label-width="80"
-              style="padding:20px 0 0 0;height:100%">
-            <Row :gutter="10">
-                 <Col span="4">
-                <Button-group style="float:left">
-                    <Button icon="compose"
-                            type="primary">导入</Button>
-                    <Button icon="document"
-                            type="success">导出</Button>
-                            <Button type="info"
-                            icon="plus"
-                            @click="createModal = true">添加</Button>
-                </Button-group>
-                </Col>
+<Form :label-width="80" style="padding:20px 0 0 0;height:100%">
+<Row :gutter="10">
+    <Col span="4">
+    <Button-group style="float:left">
+<Button icon="compose" type="ghost">导入</Button>
+<Button icon="document" type="ghost">导出</Button>
+<Button type="ghost" icon="plus" @click="createModal = true">添加</Button>
+</Button-group>
+</Col>
 
-                <Col span="5"offset="3">
-                <Form-item label="单位">
-                    <Input v-model="company_name" placeholder="请输入"></Input>
-                </Form-item>
-                </Col>
-                <Col span="5">
-                <Form-item label="车型">
-                    <Select v-model="car_type">
+<Col span="5" offset="3">
+<Form-item label="单位">
+    <Select v-model="company_code">
+                        <Option v-for="item in companies"
+                                :value="item.value"
+                                :key="item">{{ item.label }}</Option>
+                    </Select>
+</Form-item>
+</Col>
+<Col span="5">
+<Form-item label="车型">
+    <Select v-model="car_type">
                         <Option v-for="item in cars_types"
                                 :value="item.value"
                                 :key="item">{{ item.label }}</Option>
                     </Select>
-                </Form-item>
-                </Col>
-                <Col span="5">
-                <Form-item label="状态">
-                    <Select v-model="state">
+</Form-item>
+</Col>
+<Col span="5">
+<Form-item label="状态">
+    <Select v-model="state">
                         <Option v-for="item in status"
                                 :value="item.value"
                                 :key="item">{{ item.label }}</Option>
                     </Select>
-                </Form-item>
-                </Col>
+</Form-item>
+</Col>
 
-                <Col span="2" >
-                <Button-group style="float:right">
-                    
-                    <Button icon="search"
-                            type="info"
-                            @click="search(3)">查询</Button>
-                </Button-group>
-                </Col>
-            </Row>
-        </Form>
+<Col span="2">
+<Button-group style="float:right">
 
-<Table :context="self" :data="carsData" @on-current-change="selectRow" :columns="carsTable" highlight-row stripe></Table>
+<Button icon="search" type="ghost" @click="search(1)">查询</Button>
+</Button-group>
+</Col>
+</Row>
+</Form>
+
+<Table :context="self" :data="carsData" :columns="carsTable" highlight-row stripe></Table>
 <div style="margin: 10px;overflow: hidden">
 <div style="float: right;">
- <Page :total="totalNumber" :current="1" :page-size="pageSize" v-on:on-change="changePage" show-sizer></Page>
+<Page :total="totalNumber" :current="1" :page-size="pageSize" v-on:on-change="changePage" show-sizer></Page>
 </div>
 </div>
 
-        <Modal v-model="createModal"
-               title="添加"
-               @on-ok="ok"
-               @on-cancel="cancel">
-            <Form :model="createItem"
-                  :label-width="80">
-                <Form-item label="所属单位">
-                    <Input v-model="createItem.company_name"></Input>
-                </Form-item>
-                <Form-item label="车型">
-                    <Select v-model="createItem.car_type"
-                            placeholder="请选择">
-                        <Option value="beijing">北京市</Option>
-                        <Option value="shanghai">上海市</Option>
-                        <Option value="shenzhen">深圳市</Option>
+<Modal v-model="createModal" title="添加" @on-ok="addCar" @on-cancel="$Message.info('取消添加');">
+    <Form :model="createItem" :label-width="80">
+        <Form-item label="所属单位">
+            <Select v-model="createItem.company_code" placeholder="请选择">
+                        <Option v-for="item in cars_types"
+                                :value="item.value"
+                                :key="item">{{ item.label }}</Option>
+            </Select>
+        </Form-item>
+        <Form-item label="车型">
+            <Select v-model="createItem.car_type" placeholder="请选择">
+                        <Option v-for="item in cars_types"
+                                :value="item.value"
+                                :key="item">{{ item.label }}</Option>
                     </Select>
-                </Form-item>
-                <Form-item label="车牌号">
-                    <Input v-model="createItem.car_plate"></Input>
-                </Form-item>
-                <Form-item label="座位数">
-                    <Input v-model="createItem.seat_count"></Input>
-                </Form-item>
-                <Form-item label="司机姓名">
-                    <Input v-model="createItem.driver_name"></Input>
-                </Form-item>
-                <Form-item label="司机手机号">
-                    <Input v-model="createItem.driver_mobile"></Input>
-                </Form-item>
-                <Form-item label="状态">
-                    <i-switch v-model="createItem.state"
-                            size="large">
-                        <span slot="open">启用</span>
-                        <span slot="close">停用</span>
-                    </i-switch>
-                </Form-item>
-            </Form>
-        </Modal>
+        </Form-item>
+        <Form-item label="车牌号">
+            <Input v-model="createItem.car_plate"></Input>
+        </Form-item>
+        <Form-item label="座位数">
+            <Input v-model="createItem.seat_count"></Input>
+        </Form-item>
+        <Form-item label="司机姓名">
+            <Input v-model="createItem.driver"></Input>
+        </Form-item>
+        <Form-item label="司机手机号">
+            <Input v-model="createItem.driver_mobile"></Input>
+        </Form-item>
+        <Form-item label="状态">
+            <i-switch v-model="createItem.state" size="large">
+                <span slot="open">启用</span>
+                <span slot="close">停用</span>
+            </i-switch>
+        </Form-item>
+    </Form>
+</Modal>
 
 </div>
 </template>
@@ -108,55 +103,75 @@
     export default {
         data() {
             return {
-              cars_types:[
-                {
-                    value: '1',
-                    label: '大型'
-                },
-                {
-                    value: '2',
-                    label: '中型'
-                },
-                {
-                    value: '3',
-                    label: '商务车'
-                },
-                {
-                    value: '4',
-                    label: '轿车'
-                }],
-                status:[
-                {
-                    value: '1',
-                    label: '启用'
-                },
-                {
-                    value: '0',
-                    label: '停用'
-                },
-                {
-                    title: '操作',
-                    key: 'action',
-                    render() {
-                        return `<Button-group> <i-button type="error" >删除</i-button><i-button type="info" >编辑</i-button></Button-group>`;
+                companies: [
+                    {
+                        value: 'aaa',
+                        label: 'A运输'
+                    },
+                    {
+                        value: 'bbb',
+                        label: 'B运输'
+                    },
+                    {
+                        value: 'ccc',
+                        label: 'C运输'
+                    },
+                    {
+                        value: 'ddd',
+                        label: 'D运输'
+                    },
+                    {
+                        value: 'eee',
+                        label: 'E运输'
                     }
-                }],
-                company_name:'',
-                car_type:'',
-                state:'',
+                ],
+                cars_types: [
+                    {
+                        value: '1',
+                        label: '大型'
+                    },
+                    {
+                        value: '2',
+                        label: '中型'
+                    },
+                    {
+                        value: '3',
+                        label: '商务车'
+                    },
+                    {
+                        value: '4',
+                        label: '轿车'
+                    }],
+                status: [
+                    {
+                        value: '1',
+                        label: '启用'
+                    },
+                    {
+                        value: '0',
+                        label: '停用'
+                    }
+                    ],
+
+                //条件查询
+                company_code: '',
+                car_type: '',
+                state: '',
+
+                //添加model
                 createItem:
                 {
-                    company_name:'',
-                    car_type:'',
-                    car_plate:'',
-                    seat_count:'',
-                    driver_name:'',
-                    driver_mobile:'',
-                    state:''
+                    company_code: '',
+                    car_type: '',
+                    car_plate: '',
+                    seat_count: '',
+                    driver: '',
+                    driver_mobile: '',
+                    state: ''
                 },
-                createModal:false,
+                createModal: false,
 
-                self:this,
+                self: this,
                 pageSize: 10,
                 totalNumber: 10,
                 carsData: [],
@@ -168,7 +183,7 @@
                     },
                     {
                         title: '所属单位',
-                        key: 'company_name'
+                        key: 'company_code'
                     },
                     {
                         title: '车辆类型',
@@ -184,7 +199,7 @@
                     },
                     {
                         title: '司机姓名',
-                        key: 'driver_name'
+                        key: 'driver'
                     },
                     {
                         title: '司机手机号',
@@ -198,33 +213,55 @@
                             const text = row.state == 1 ? '启用' : '停用';
                             return `<tag type="dot" color="${color}">${text}</tag>`;
                         }
-                    }]
+                    },
+                    {
+                    title: '操作',
+                    key: 'action',
+                    render() {
+                        return `<Button-group> <i-button @click="delCar" type="ghost" style="color:red">删除</i-button><i-button type="ghost" >编辑</i-button></Button-group>`;
+                    }
+                }]
             }
         },
         methods: {
-        ok() {
-            this.$Message.info('确认添加');
-        },
-        cancel() {
-            this.$Message.warning('取消添加');
-        },
-        search(pageNumber) {
-            this.$http.get('http://localhost:3000/stations',{
-                params:{
-                    _page:pageNumber,
-                    _limit:this.pageSize,
-                    company_name:this.company_name,
-                    car_type:this.car_type,
-                    state:this.state
+            search(pageNumber) {
+                var params = {};
+                params["_page"] = pageNumber;
+                params["_limit"] = this.pageSize;
+                if (this.company_code != '') {
+                    params["company_code"] = this.company_code;
                 }
-            } ).then((res) => {
-                this.totalNumber =Number(res.headers['map']['X-Total-Count'][0]) 
-                this.tableContent = res.body;
-            });
-        },
-        changePage:function(pageNumber) {
-            this.search(pageNumber);
-        }
+                if (this.car_type != '') {
+                    params["car_type"] = this.car_type;
+                }
+                if (this.state != '') {
+
+                    params["state"] = this.state;
+                }
+                this.$http.get('http://localhost:3000/cars', {
+                    params: params
+                }).then((res) => {
+                    this.totalNumber = Number(res.headers['map']['X-Total-Count'][0])
+                    this.carsData = res.body;
+                });
+            },
+            changePage: function (pageNumber) {
+                this.search(pageNumber);
+            },
+            delCar() {
+                this.$Modal.confirm({
+                    content: '<h3>确认删除该车辆吗</h3>',
+                    onOk: () => {
+                        this.$Message.info('删除了车辆');
+                    },
+                    onCancel: () => {
+                        this.$Message.info('取消删除');
+                    }
+                });
+            },
+            addCar() {
+
+            }
         }
     }
 </script>
